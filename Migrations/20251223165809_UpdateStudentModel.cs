04d8f57c04b522aaa91f3ff582991896ce9232c6
abdikafi4455-wq <abdikafi4455@gmail.com>
2026-05-13 18:29:18 -0700

    kaafi

---
+++ Migrations/20251223165809_UpdateStudentModel.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ASPEFCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateStudentModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Studentstbl",
+                table: "Studentstbl");
+
+            migrationBuilder.RenameTable(
+                name: "Studentstbl",
+                newName: "Students");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Mobile",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Students",
+                table: "Students",
+                column: "ID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Students",
+                table: "Students");
+
+            migrationBuilder.RenameTable(
+                name: "Students",
+                newName: "Studentstbl");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Name",
+                table: "Studentstbl",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Mobile",
+                table: "Studentstbl",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Studentstbl",
+                table: "Studentstbl",
+                column: "ID");
+        }
+    }
+}
